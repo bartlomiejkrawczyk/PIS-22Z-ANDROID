2c3903e3e4c0aa2602cb0f6c15143259d440e8f8
mbrzozow <01158596@pw.edu.pl>
2022-12-17 20:10:22 +0100

    [AN-18] Add nav button

---
+++ app/src/main/java/com/example/android/ui/MainMenuActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.android.R;
+import com.example.android.databinding.ActivityMainMenuBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+	public ActivityMainMenuBinding binding;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+
+		BottomNavigationView navView = findViewById(R.id.nav_view);
+		// Passing each menu ID as a set of Ids because each
+		// menu should be considered as top level destinations.
+		AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+				R.id.navigation_menu, R.id.navigation_tests, R.id.navigation_add, R.id.navigation_settings)
+				.build();
+		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_menu);
+//		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+		NavigationUI.setupWithNavController(binding.navView, navController);
+	}
+
+}
