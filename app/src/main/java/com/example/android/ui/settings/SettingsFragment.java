8066ddba8d827a234e41f6d89668935f1aa5ddce
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-18 21:09:26 +0100

    [AN-34] Define more settings

--- app/src/main/java/com/example/android/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/android/ui/settings/SettingsFragment.java
@@ -1,34 +1,13 @@
 package com.example.android.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import com.example.android.databinding.FragmentSettingsBinding;
+import androidx.preference.PreferenceFragmentCompat;
+import com.example.android.R;
 
-public class SettingsFragment extends Fragment {
-
-	private FragmentSettingsBinding binding;
-
-	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		SettingsViewModel settingsViewModel =
-				new ViewModelProvider(this).get(SettingsViewModel.class);
-
-		binding = FragmentSettingsBinding.inflate(inflater, container, false);
-		View root = binding.getRoot();
-
-		final TextView textView = binding.textSettings;
-		settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-		return root;
-	}
+public class SettingsFragment extends PreferenceFragmentCompat {
 
 	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		binding = null;
+	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+		setPreferencesFromResource(R.xml.root_preferences, rootKey);
 	}
 }
