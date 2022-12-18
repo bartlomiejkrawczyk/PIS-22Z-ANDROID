707f704fce69f2baef323a8fb429b6b579f48c21
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2022-12-19 00:21:12 +0100

    [AN-34] Implement logout

--- app/src/main/java/com/example/android/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/android/ui/settings/SettingsFragment.java
@@ -1,13 +1,24 @@
 package com.example.android.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import com.example.android.R;
+import com.example.android.ui.login.LoginActivity;
+import java.util.Optional;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
 	@Override
 	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 		setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+		Optional<Preference> preference = Optional.ofNullable(findPreference("logout"));
+		preference.ifPresent(p -> p.setOnPreferenceClickListener(pref -> {
+			var intent = new Intent(getContext(), LoginActivity.class);
+			startActivity(intent);
+			return true;
+		}));
 	}
 }
