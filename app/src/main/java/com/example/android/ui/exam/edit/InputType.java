c9746587015d3e6794c57ef296c3ae51f6ec2b5a
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-01-06 00:50:42 +0100

    [AN-27] Define three exercise types

---
+++ app/src/main/java/com/example/android/ui/exam/edit/InputType.java
@@ -0,0 +1,28 @@
+package com.example.android.ui.exam.edit;
+
+import com.example.model.exam.Choice;
+import com.example.model.exam.Exercise;
+import com.example.model.exam.FillBlanks;
+import com.example.model.exam.FlashCard;
+import com.example.model.exam.MultipleChoice;
+import com.example.model.exam.MultipleTruthOrFalse;
+import com.example.model.exam.SelectFromList;
+import com.example.model.exam.TruthOrFalse;
+import java.util.List;
+
+public enum InputType {
+
+	SIMPLE(List.of(FlashCard.class)),
+	CHOICE(List.of(Choice.class, MultipleChoice.class, TruthOrFalse.class, MultipleTruthOrFalse.class)),
+	PARAGRAPH(List.of(FillBlanks.class, SelectFromList.class));
+
+	private final List<Class<? extends Exercise>> classes;
+
+	InputType(List<Class<? extends Exercise>> classes) {
+		this.classes = classes;
+	}
+
+	public List<Class<? extends Exercise>> getClasses() {
+		return classes;
+	}
+}
