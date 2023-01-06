2b754ddc0d018875dac7d7569000331e1cfe6b62
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-01-06 14:04:58 +0100

    [AN-44] Finish basic view

--- app/src/main/java/com/example/android/ui/section/edit/EditSectionActivity.java
+++ app/src/main/java/com/example/android/ui/section/edit/EditSectionActivity.java
@@ -6,10 +6,14 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.android.databinding.ActivityEditSectionBinding;
 import com.example.android.ui.ConceptActivity;
+import com.example.android.ui.MainMenuActivity;
 import com.example.android.ui.exam.edit.EditExerciseActivity;
 import com.example.android.ui.section.SectionActivity;
 
 public class EditSectionActivity extends AppCompatActivity {
+
+	private Button buttonDone;
+	private Button buttonCancel;
 
 	private Button buttonAddConcept;
 	private Button buttonAddSubSection;
@@ -28,6 +32,8 @@ public class EditSectionActivity extends AppCompatActivity {
 		parentSectionId = previousIntent.getIntExtra(SectionActivity.ARG_PARENT_SECTION_ID, 0);
 		sectionId = previousIntent.getIntExtra(SectionActivity.ARG_SECTION_ID, 0);
 
+		this.buttonDone = binding.buttonDone;
+		this.buttonCancel = binding.buttonCancel;
 		this.buttonAddConcept = binding.buttonAddConcept;
 		this.buttonAddSubSection = binding.buttonAddSubSection;
 		this.buttonAddExercise = binding.buttonAddExercise;
@@ -36,6 +42,15 @@ public class EditSectionActivity extends AppCompatActivity {
 	}
 
 	private void setupListeners() {
+		buttonDone.setOnClickListener(v -> finish());
+
+		buttonCancel.setOnClickListener(v -> {
+			Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			// TODO: delete section
+			startActivity(intent);
+		});
+
 		buttonAddConcept.setOnClickListener(v -> {
 			var intent = new Intent(this, ConceptActivity.class);
 			intent.putExtra(SectionActivity.ARG_SECTION_ID, sectionId);
@@ -47,7 +62,6 @@ public class EditSectionActivity extends AppCompatActivity {
 			var intent = new Intent(this, EditSectionActivity.class);
 			intent.putExtra(SectionActivity.ARG_PARENT_SECTION_ID, sectionId);
 			intent.putExtra(SectionActivity.ARG_SECTION_ID, 0);
-			// TODO: open in edit view
 			startActivity(intent);
 		});
 
